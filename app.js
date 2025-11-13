c8bfc3d8faf968c4d90c608560d72cafdf83d757
Charlene (Chay) Makwara <158230758+MakChay@users.noreply.github.com>
2025-11-13 03:21:02 +0000

    Keys

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ import { getFirestore, collection, addDoc, doc, getDocs, setDoc, deleteDoc, quer
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCcyJOa4KzzrIlv8URPqURIjlIUZg-08BE",
-  authDomain: "personal-media-tracker-a8d75.firebaseapp.com",
-  projectId: "personal-media-tracker-a8d75",
-  storageBucket: "personal-media-tracker-a8d75.firebasestorage.app",
-  messagingSenderId: "1077955225349",
-  appId: "1:1077955225349:web:5ba58400a65a87eb5f796c",
-  measurementId: "G-9K5KYRVF1T"
+apiKey: import.meta.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: import.meta.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: import.meta.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -20,6 +20,8 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, collection, addDoc, getDocs, query, where };
 
 document.addEventListener('DOMContentLoaded', () => {
   const welcomeMessage = document.getElementById('welcomeMessage');
