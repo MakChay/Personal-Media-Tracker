36c56ffce1d290e7f883f6de4c625aa4c1b782b5
Charlene (Chay) Makwara <158230758+MakChay@users.noreply.github.com>
2025-11-13 04:22:39 +0000

    Remove Firebase Analytics

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 // app.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 import { getFirestore, collection, addDoc, doc, getDocs, setDoc, deleteDoc, query, where } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
 import { firebaseConfig } from "./config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
